0d45ef365725a204a1fff61cd2c86b42de297800
Paulo Adeva <paulo.adeva@gmail.com>
2022-04-08 23:34:27 +1000

    confused about theme

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+const button = document.querySelector('.toggle');
+
+
 function setTheme() {
     const root = document.documentElement;
     const newTheme = root.className === 'dark' ? 'light' : 'dark';
     root.className = newTheme;
-}
 
-document.querySelector('.toggle').addEventListener('click', setTheme)
+
+    if (button.textContent = 'dark_mode'){
+        button.textContent = 'light_mode'
+    }
+    else if (button.textContent = 'light_mode') {
+        button.textContent = 'dark_mode'
+    };
+
+    console.log(button.textContent)
+
+}
+button.addEventListener('click', setTheme)
